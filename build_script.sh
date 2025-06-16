f51636bc1a5275b72b46fe6648a6ab80a6b112e4
karthikeyan sampathkumar <karthik19944@gmail.com>
2025-06-16 13:34:12 +0000

    Add build script for Jenkins

---
+++ build_script.sh
@@ -0,0 +1,8 @@
+# build_script.sh
+#!/bin/bash
+echo "--- Building the project ---"
+echo "Running tests..."
+# Simulate some tests
+sleep 2
+echo "Tests passed!"
+echo "Build successful at $(date)"
